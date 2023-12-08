b938adad5ad77c02bffadc3697e4b959662ac128
Lucius-lsr <462114286@qq.com>
2023-12-08 04:37:46 +0000

    none

--- scripts/eval_commonsense.sh
+++ scripts/eval_commonsense.sh
@@ -1,11 +1,11 @@
 export PYTHONPATH='.'
 
 base_model=baffo32/decapoda-research-llama-7B-hf
-pretrained_dir=output/Compresso-finetune-s50.0-lr1e-05-reglr0.1-warmup0/distill_alpaca
+pretrained_dir=output/Compresso-finetune-s50.0-lr5e-05-reglr0.1-warmup0/2023-12-7-3-43
 prompt_mark=0 # 0: do not add pruning prompt during evaluation; 1: add the pruning prompt same as training; 2. add the pruning prompt for evaluation
-lora_param=Q.V # the lora param in training
+lora_param=Q.V.K.F.O # the lora param in training
 
-for e in epoch0 epoch1 epoch2
+for e in epoch2
 do
     pretrained_path=$pretrained_dir/$e
     file_name=$(echo $pretrained_path | cut -d'/' -f $(($(echo $pretrained_path | tr '/' '\n' | wc -l) - 2)))
--- test_lora.py
+++ test_lora.py
@@ -2,6 +2,7 @@ import torch
 import os
 import numpy as np
 
-lora = torch.load('output/Compresso-finetune-s50.0-lr5e-05-reglr0.1-warmup0/2023-12-1-11-48/epoch0/lora_weights.pt', map_location="cpu")
+lora = torch.load('output/Compresso-finetune-s50.0-lr5e-05-reglr0.1-warmup0/2023-12-7-3-43/epoch0/lora_weights.pt', map_location="cpu")
 
-print(lora)
+for k in lora.keys():
+    print(k, lora[k].mean())
