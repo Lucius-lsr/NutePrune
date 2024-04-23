aac581027179dabc612401a666abc4f0d10ba2c9
Shengrui Li <46862479+Lucius-lsr@users.noreply.github.com>
2024-04-23 23:34:00 +0800

    Delete test_lora.py

--- test_lora.py
+++
@@ -1,8 +0,0 @@
-import torch
-import os
-import numpy as np
-
-lora = torch.load('output/NutePrune-alternative-s50.0-lr5e-06-reglr0.2-warmup1/2024-1-10-3-4/epoch0/lora_weights.pt', map_location="cpu")
-
-for k in lora.keys():
-    print(k, lora[k].mean())
